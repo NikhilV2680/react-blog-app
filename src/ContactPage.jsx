f9c5b216e96477433f31a64d1ee71be99f89ba73
NikhilV2680 <142258459+NikhilV2680@users.noreply.github.com>
2026-04-08 11:41:04 -0400

    Create ContactPage.jsx

---
+++ src/ContactPage.jsx
@@ -0,0 +1,10 @@
+function ContactPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Contact Page</h1>
+      <p>This is the contact page.</p>
+    </div>
+  );
+}
+
+export default ContactPage;
